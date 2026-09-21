708447a826fd683dfcd02e606e45e21c59f0d748
Shadab Sadat <shadabsadat21@gmail.com>
2026-09-22 00:37:23 +0430

    Add Like Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,16 @@
-import BillingCard from "./components2/BillingCard";
+import { useState } from "react";
 
 function App() {
-  const billingInformation = [
-    {
-      id: 1,
-      name: "Oliver Liam",
-      company: "Viking Burrito",
-      email: "oliver@burrito.com",
-      vat: "FRB1235476",
-    },
-    {
-      id: 2,
-      name: "Lucas Harper",
-      company: "Stone Tech Zone",
-      email: "lucas@stone-tech.com",
-      vat: "FRB1235476",
-    },
-    {
-      id: 3,
-      name: "Ethan James",
-      company: "Fiber Notion",
-      email: "ethan@fiber.com",
-      vat: "FRB1235476",
-    },
-  ];
+  const [likes, setLikes] = useState(0);
 
   return (
-    <main className="min-h-screen bg-gray-100 px-4 py-8">
-      <section className="mx-auto max-w-5xl rounded-2xl bg-white p-6 shadow-sm sm:p-8">
+    <div>
+      <p>❤️ Likes: {likes}</p>
 
-        <h1 className="mb-8 text-lg font-semibold text-slate-700">
-          Billing Information
-        </h1>
-
-        <div className="space-y-6">
-          {billingInformation.map((person) => (
-            <BillingCard
-              key={person.id}
-              name={person.name}
-              company={person.company}
-              email={person.email}
-              vat={person.vat}
-            />
-          ))}
-        </div>
-
-      </section>
-    </main>
+      <button onClick={() => setLikes(likes + 1)}>
+        Like ❤️
+      </button>
+    </div>
   );
 }
 
